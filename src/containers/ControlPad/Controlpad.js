a0f72136c2ad7808b13bb69d234ab14f7d2f4fcc
Mikie Smit <msmit1123@gmail.com>
2019-10-24 08:55:29 -0500

    added icons to toggle buttons

--- src/containers/ControlPad/Controlpad.js
+++ src/containers/ControlPad/Controlpad.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlayCircle,
+  faCog,
+  faMicrophoneAlt,
+  faPowerOff
+} from '@fortawesome/free-solid-svg-icons';
 
 import './Controlpad.scss';
 
@@ -8,24 +15,30 @@ function Controlpad(props) {
   return (
     <div className='controls'>
       <div className='controls__module'>
-        <ToggleSwitch id='power' className='toggle-switch-type-B'>
-          <span>O</span>
+        <ToggleSwitch id='play' className='toggle-switch-type-C'>
+          <span>
+            <FontAwesomeIcon icon={faPlayCircle} />
+          </span>
+        </ToggleSwitch>
+        <ToggleSwitch id='record' className='toggle-switch-type-B'>
+          <span>
+            <FontAwesomeIcon icon={faMicrophoneAlt} />
+          </span>
         </ToggleSwitch>
         <ToggleSwitch id='settings' className='toggle-switch-type-B'>
-          <span>G</span>
+          <span>
+            <FontAwesomeIcon icon={faCog} />
+          </span>
+        </ToggleSwitch>
+        <ToggleSwitch id='power' className='toggle-switch-type-B'>
+          <span>
+            <FontAwesomeIcon icon={faPowerOff} />
+          </span>
         </ToggleSwitch>
       </div>
       <div className='controls__module'>
         <div>current time / max time</div>
         <div>current time slider</div>
-      </div>
-      <div className='controls__module'>
-        <ToggleSwitch id='record' className='toggle-switch-type-B'>
-          <span>R</span>
-        </ToggleSwitch>
-        <ToggleSwitch id='play' className='toggle-switch-type-B'>
-          <span>P</span>
-        </ToggleSwitch>
       </div>
       <div className='controls__module'>
         <div>volume slider</div>
