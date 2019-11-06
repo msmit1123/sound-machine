cf76e6cb6bd21bf978105df38e5758bb4cb45901
Mikie Smit <msmit1123@gmail.com>
2019-11-05 18:50:41 -0600

    added big demo 2 library

--- src/initialSoundLibrary.js
+++ src/initialSoundLibrary.js
@@ -88,69 +88,390 @@ export const demo2SoundLibrary = [
   [
     {
       pressKey: '1',
-      title: 'chord 1',
+      title: 'C3',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__c3.mp3',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+      speed: 100
     },
     {
       pressKey: 'q',
-      title: 'chord 2',
+      title: 'Chord 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/guitar-chords/guitar-chord-1.wav',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+      speed: 100
     },
     {
       pressKey: 'a',
-      title: 'chord 3',
+      title: 'Kick 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/kicks/TSB%20KICK%20(1).wav',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+      speed: 100
     },
     {
       pressKey: 'z',
-      title: 'closed HH',
+      title: 'Kick 3',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/kicks/TSB%20KICK%20(3).wav',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+      speed: 100
+    },
+    {
+      pressKey: 'A',
+      title: 'Kick 4',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/kicks/TSB%20KICK%20(4).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'Z',
+      title: 'Kick 5',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/kicks/TSB%20KICK%20(5).wav',
+      volume: 100,
+      speed: 100
     }
   ],
   [
     {
       pressKey: '2',
-      title: 'open HH',
+      title: 'D3',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__d3.mp3',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+      speed: 100
     },
     {
       pressKey: 'w',
-      title: 'punchy kick',
+      title: 'Chord 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/guitar-chords/guitar-chord-2.wav',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+      speed: 100
     },
     {
       pressKey: 's',
-      title: 'side stick',
+      title: 'HH closed 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/hats/TSB%20HAT%20C%20(3).wav',
       volume: 100,
-      speed: 100,
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+      speed: 100
     },
     {
       pressKey: 'x',
-      title: 'snare',
-      volume: 100,
-      speed: 100,
+      title: 'HH open 1',
       url:
-        'http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Instruments/Drums/Hihats/idg_Hi_H-intermed-2283/idg_Hi_H-intermed-2283_hifi.mp3'
+        'http://www.mikiesmit.com/libraries/audio/drums/hats/TSB%20HAT%20O%20(3).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'S',
+      title: 'HH closed',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/hats/TSB%20HAT%20C%20(3).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'X',
+      title: 'HH closed',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/hats/TSB%20HAT%20O%20(3).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '3',
+      title: 'E3',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__e3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'e',
+      title: 'chord 3',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/guitar-chords/guitar-chord-3.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'd',
+      title: 'snare 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/snares/TSB%20SNARE%20(1).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'c',
+      title: 'tom 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TOM%20(1).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'D',
+      title: 'triangle 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TRIANGLE%20(1).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'C',
+      title: 'bongo 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20BONGO%20(1).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '4',
+      title: 'F3',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__f3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'r',
+      title: 'chord 4',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/guitar-chords/guitar-chord-4.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'f',
+      title: 'snare 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/snares/TSB%20SNARE%20(2).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'v',
+      title: 'tom 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TOM%20(2).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'F',
+      title: 'triangle 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TRIANGLE%20(2).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'V',
+      title: 'bongo 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20BONGO%20(2).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '5',
+      title: 'G3',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__g3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 't',
+      title: 'chord 16',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/guitar-chords/guitar-chord-16.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'g',
+      title: 'snare 3',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/snares/TSB%20SNARE%20(3).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'b',
+      title: 'tamb 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TAMBOURINE%20(1).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'G',
+      title: 'clank',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20CLANCK.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'B',
+      title: 'bongo 3',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20BONGO%20(3).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '6',
+      title: 'A4',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__a3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'y',
+      title: 'chord A',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'h',
+      title: 'snare 6',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/snares/TSB%20SNARE%20(6).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'n',
+      title: 'tamb 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20TAMBOURINE%20(2).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'H',
+      title: 'clap 1',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/claps/TSB%20CLAP%20(10).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'N',
+      title: 'bongo 4',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20BONGO%20(4).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '7',
+      title: 'B4',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__b3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'u',
+      title: 'chord B',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'j',
+      title: 'snare 9',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/snares/TSB%20SNARE%20(9).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'm',
+      title: 'static',
+      url: 'http://www.mikiesmit.com/libraries/audio/effects/radio-static.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'J',
+      title: 'clap 2',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/claps/TSB%20CLAP%20(12).wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'M',
+      title: 'bongo 5',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/percs/TSB%20BONGO%20(5).wav',
+      volume: 100,
+      speed: 100
+    }
+  ],
+  [
+    {
+      pressKey: '8',
+      title: 'C4',
+      url: 'http://www.mikiesmit.com/libraries/audio/pianoShort/Piano__c4.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'i',
+      title: 'chord c',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'k',
+      title: 'Bell',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/sounds/TSB%20BELL.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: ',',
+      title: 'M. Kick',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/sounds/TSB%20MASSIVE%20KICK.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: 'K',
+      title: 'FX',
+      url: 'http://www.mikiesmit.com/libraries/audio/drums/sounds/TSB%20FX.wav',
+      volume: 100,
+      speed: 100
+    },
+    {
+      pressKey: '<',
+      title: 'Swoosh',
+      url:
+        'http://www.mikiesmit.com/libraries/audio/drums/sounds/TSB%20THLUCK.wav',
+      volume: 100,
+      speed: 100
     }
   ]
 ];
-export const demo2SoundLoop = [
-  { time: 0, column: 1, row: 1 },
-  { time: 500, column: 1, row: 0 },
-  { time: 700, column: 1, row: 1 },
-  { time: 1150, column: 1, row: 1 },
-  { time: 1400, column: 1, row: 0 }
-];
+
+export const demo2SoundLoop = [];
