225ad45adaa252e2060d44fc1e8a8c535f4d7d18
Mikie Smit <msmit1123@gmail.com>
2019-11-20 06:43:24 -0600

    added minimum characters setting

--- src/components/AutoCompleteTextInput/AutoCompleteInterface.js
+++ src/components/AutoCompleteTextInput/AutoCompleteInterface.js
@@ -35,27 +35,30 @@ const autoCompleteInterface = {
   ) => {
     // create a client side options object to either populate with specified request options or defaults
     let options = requestOptions ? requestOptions : {};
-    options.minChars = options.minChars ? options.minChars : 1; //set the minimum number of characters required to trigger a call
+    options.minChars = options.minChars ? options.minChars : 2; //set the minimum number of characters required to trigger a call
     options.delay = options.delay ? options.delay : 500; //set the delay between calls so it doesn't spam the server
-    options.timerLoc = options.timerLoc ? options.timerLoc : window; //set a location to attach the timer to. Ideally the input being autofilled is passed in. as a fallback, attach to the window.
+    options.timerLoc = options.timerLoc ? options.timerLoc : window; //set a location to attach the timer to. Ideally the timer is attached to the input being autofilled. as a fallback, attach to the window.
 
     //if a timer was already running, clear it
     if (options.timerLoc.timer !== null) {
       clearTimeout(options.timerLoc.timer);
     }
 
-    // create a promise and link it to the timer
-    let promise = new Promise((resolve, reject) => {
-      //start a new client side timer in case user is typing, server doesn't get spammed with requests until user stops typing, at which point it only sends the final request
-      try {
-        options.timerLoc.timer = setTimeout(() => {
-          resolve(autoCompleteInterface.fetchData(urlToAPI, requestDataObj));
-        }, options.delay);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-    return promise;
+    //only bother making a request if enough characters are typed
+    if (requestDataObj.term.length >= options.minChars) {
+      // create a promise and link it to the timer
+      let promise = new Promise((resolve, reject) => {
+        //start a new client side timer in case user is typing, server doesn't get spammed with requests until user stops typing, at which point it only sends the final request
+        try {
+          options.timerLoc.timer = setTimeout(() => {
+            resolve(autoCompleteInterface.fetchData(urlToAPI, requestDataObj));
+          }, options.delay);
+        } catch (error) {
+          reject(console.log(error));
+        }
+      });
+      return promise;
+    }
   }
 };
 
